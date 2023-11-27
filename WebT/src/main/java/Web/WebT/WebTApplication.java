f9ae4903a9e42d04494bf130056ec8fd34128825
Korol <92019385+eucalypt63@users.noreply.github.com>
2023-11-27 12:57:00 +0300

    Add files via upload

---
+++ WebT/src/main/java/Web/WebT/WebTApplication.java
@@ -0,0 +1,13 @@
+package Web.WebT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTApplication.class, args);
+	}
+
+}
